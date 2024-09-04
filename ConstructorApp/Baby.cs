8ccac4eff200016a1b98ef07e81b4342244728ba
KAAN <alikaanozoll@gmail.com>
2024-09-04 13:05:26 +0300

    Add project files.

---
+++ ConstructorApp/Baby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorApp
+{
+    public class Baby
+    {   //Özellikler || Proporties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateofBirth { get; set; }
+
+        // Default Constructor
+        public Baby()
+        {
+            DateofBirth = DateTime.Now;
+            Console.WriteLine("Ingaaaaa,bebek doğdu :)\n");
+
+        }
+
+        // Parametre Alan Constructor
+        public Baby(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            DateofBirth = DateTime.Now;
+            Console.WriteLine("Ingaaaaa,bebek doğdu :)\n");
+
+        }
+        //Bebeğe dair bilgileri görüntüleme || Method to display baby's details
+        public void ShowInformation()
+        {
+            Console.WriteLine($"Doğan bebeğimizin Adı : {Name}, Soyadı : {Surname}, Doğum Tarihi : {DateofBirth}\n");
+        }
+    }
+}
---
+++ ConstructorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConstructorApp;
+// // Default constructor ile bebek nesnesi oluşturma || Creating a baby object with the default constructor
+
+Baby baby = new Baby();
+baby.ShowInformation();
+
+// Parametreli constructor ile bebek nesnesi oluşturma || // Creating a baby object with parameterized constructor
+
+Baby baby2 = new Baby("Harvey","Specter");
+baby2.ShowInformation();
